188986995163ccb7fadb19455a1587f61d4a337b
gbahugun <gaganbahugun@hotmail.com>
2018-02-18 21:32:30 -0800

    added models

---
+++ LmycWebSite/Migrations/201802190527386_Lymc.cs
@@ -0,0 +1,16 @@
+namespace LmycWebSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Lymc : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
